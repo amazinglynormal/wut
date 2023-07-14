713219c72b0887fdbcd6da300a7c1dd37abd4f08
Stephen <stephenbrady365@gmail.com>
2023-07-14 17:47:30 +0100

    Basic hero section

---
+++ app/hero-section/HeroSection.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import styles from "./HeroSection.module.css";
+
+export default function HeroSection() {
+  return (
+    <section className={styles.section}>
+      <h1 className={styles.heading}>This is a hero section</h1>
+      <p className={styles.subText}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Itaque
+        dignissimos delectus ex, sapiente quos doloribus tempora, culpa aliquid
+        ea placeat fugit voluptatem pariatur praesentium neque?
+      </p>
+      <div className={styles.buttonContainer}>
+        <button
+          type="button"
+          className={`${styles.button} ${styles.primaryButton}`}
+        >
+          Call-to-Action Button
+        </button>
+        <button
+          type="button"
+          className={`${styles.button} ${styles.secondaryButton}`}
+        >
+          Goes to pricing <span className={styles.buttonSpan}>→</span>
+        </button>
+      </div>
+    </section>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Nav from "./components/Nav";
+import HeroSection from "./hero-section/HeroSection";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Nav />
+      <HeroSection />
     </main>
   );
 }
